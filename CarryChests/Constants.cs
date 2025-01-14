ab7866b95cbe3bfdb70c9d2075a9350af9c084b8
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2025-01-13 16:36:35 -0800

    Carry Chests 1.2.0 (#10)
    * Update fr.json (Carry Chests, 1.1 update) (#7)
    
    Update on the lines added
    
    * Update to version 1.2.0-beta.1 with new features
    
    - Renamed and added constants in `Constants.cs` for Better Chests integration.
    - Introduced `GrabEmptyAsItem` and `SwapChests` properties in `ModConfig`.
    - Added `carry_chests` console command in `ModEntry` for managing backups and help.
    - Implemented slowness effect management in `OnOneSecondUpdateTicked`.
    - Updated `ConfigMenu` to include new configuration options.
    - Added patches in `ModPatches.cs` for `InventoryMenu` interactions.
    - Renamed `InventoryHelper` to `ModExtensions` and updated methods.
    - Updated multiple language files for new features and commands.
    - Updated `FauxCore` subproject to a new commit hash.
    
    * ru.json (#8)
    
    Co-authored-by: Matthew <3005344+LeFauxMatt@users.noreply.github.com>
    
    * Bump version and refactor command handling
    
    - Updated version number to `1.2.0-beta.2`.
    - Refactored constant keys in `Constants.cs` for maintainability.
    - Cleaned up `using` directives in `ModEntry.cs`.
    - Implemented `CommandHelper` for better command organization.
    - Added `OnCommandReceived` method for improved command handling.
    - Updated localization strings in `default.json` for clarity.
    - Updated subproject commit reference in `FauxCore`.
    
    * Update help command description in localization files
    
    The description for the command help has been changed from "Lists all available commands." to "Commands:" across multiple language files. This update standardizes the phrasing used in the help command, making it more concise and uniform across different translations.
    
    * Fix GitHub repository path in CarryChests.csproj
    
    Updated the `<UpdateKeys>` element to reflect the correct
    GitHub repository path from `LeFauxMatt/CarryChest` to
    `LeFauxMatt/CarryChests`.
    
    * Bump version to 1.2.0 for stable release
    
    Updated the version number in CarryChests.csproj from
    1.2.0-beta.2 to 1.2.0, marking the transition to a stable
    release version.
    
    ---------
    
    Co-authored-by: Caranud <caranud@gmail.com>
    Co-authored-by: MyNameRoman <157910852+MyNameRoman@users.noreply.github.com>

--- CarryChests/Constants.cs
+++ CarryChests/Constants.cs
@@ -2,15 +2,19 @@ namespace LeFauxMods.CarryChest;
 
 internal static class Constants
 {
-    public const string ColorKey = Prefix + "PlayerChoiceColor";
+    public const string BetterChestsColorKey = BetterChestsPrefix + "PlayerChoiceColor";
 
-    public const string FridgeKey = Prefix + "Fridge";
+    public const string BetterChestsFridgeKey = BetterChestsPrefix + "Fridge";
 
-    public const string GlobalInventoryKey = Prefix + "GlobalInventoryId";
+    public const string BetterChestsPrefix = "furyx639.BetterChests-ProxyChestFactory-";
+
+    public const string GlobalInventoryId = ModId + "-Backup";
+
+    public const string BetterChestsGlobalInventoryKey = BetterChestsPrefix + "GlobalInventoryId";
 
     public const string ModId = "furyx639.CarryChests";
 
-    public const string Prefix = "furyx639.BetterChests-ProxyChestFactory-";
+    public const string Prefix = ModId + "-Chest-";
 
     public const string SlowEffectKey = ModId + "/Slow";
 }
--- CarryChests/ModEntry.cs
+++ CarryChests/ModEntry.cs
@@ -1,9 +1,12 @@
 using LeFauxMods.CarryChest.Services;
 using LeFauxMods.CarryChest.Utilities;
 using LeFauxMods.Common.Models;
+using LeFauxMods.Common.Services;
 using LeFauxMods.Common.Utilities;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI.Events;
 using StardewValley.Buffs;
+using StardewValley.Menus;
 using StardewValley.Objects;
 
 namespace LeFauxMods.CarryChest;
@@ -11,6 +14,8 @@ namespace LeFauxMods.CarryChest;
 /// <inheritdoc />
 internal sealed class ModEntry : Mod
 {
+    private CommandHelper commandHelper = null!;
+
     /// <inheritdoc />
     public override void Entry(IModHelper helper)
     {
@@ -21,12 +26,64 @@ internal sealed class ModEntry : Mod
         Log.Init(this.Monitor, ModState.Config);
         ModPatches.Apply();
 
-        // TBD: Command to access global inventory chests
+        // Commands
+        this.commandHelper = new CommandHelper(
+                helper,
+                "carry_chests",
+                I18n.Command_CarryChests_Description,
+                I18n.Command_Unknown_Description)
+            .AddCommand("help", I18n.Command_Help_Description)
+            .AddCommand("backup", I18n.Command_Backups_Description);
 
         // Events
         helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
         helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         helper.Events.GameLoop.ReturnedToTitle += this.OnReturnedToTitle;
+        ModEvents.Subscribe<CommandReceivedEventArgs>(this.OnCommandReceived);
+    }
+
+    private static void OnOneSecondUpdateTicked(object? sender, OneSecondUpdateTickedEventArgs e)
+    {
+        // Add status effect to the player
+        if (Game1.player.Items.OfType<Chest>().Count() >= ModState.Config.SlownessLimit)
+        {
+            Game1.player.applyBuff(
+                new Buff(
+                    Constants.SlowEffectKey,
+                    duration: 60_000,
+                    iconTexture: Game1.buffsIcons,
+                    iconSheetIndex: 13,
+                    effects: new BuffEffects { Speed = { ModState.Config.SlownessAmount } },
+                    displayName: I18n.Effect_Overburdened()));
+
+            Log.Trace("Adding the slowness effect");
+            return;
+        }
+
+        if (!Game1.player.hasBuff(Constants.SlowEffectKey))
+        {
+            return;
+        }
+
+        // Remove status effect from the player
+        Game1.player.buffs.Remove(Constants.SlowEffectKey);
+        Log.Trace("Removing the slowness effect");
+    }
+
+    private void OnCommandReceived(CommandReceivedEventArgs e)
+    {
+        switch (e.Command)
+        {
+            case "help":
+                Log.Info(this.commandHelper.HelpText);
+                return;
+            case "backup" when !Context.IsWorldReady:
+                Log.Warn(I18n.Alert_CommandBackup_InvalidContext());
+                return;
+            case "backup":
+                Game1.activeClickableMenu = new ItemGrabMenu(ModState.Backups);
+                return;
+        }
     }
 
     private void OnGameLaunched(object? sender, GameLaunchedEventArgs e) =>
@@ -55,13 +112,13 @@ internal sealed class ModEntry : Mod
                 return;
             }
 
-            if (InventoryHelper.SwapChest(chest))
+            if (ModState.Config.SwapChests && chest.TrySwap())
             {
                 this.Helper.Input.Suppress(e.Button);
                 return;
             }
 
-            if (InventoryHelper.PickUpChest(chest, ModState.Config.TotalLimit))
+            if (chest.TryCarry())
             {
                 this.Helper.Input.Suppress(e.Button);
                 _ = chest.Location.Objects.Remove(chest.TileLocation);
@@ -93,34 +150,6 @@ internal sealed class ModEntry : Mod
         }
     }
 
-    private static void OnOneSecondUpdateTicked(object? sender, OneSecondUpdateTickedEventArgs e)
-    {
-        // Add status effect to the player
-        if (Game1.player.Items.OfType<Chest>().Count() >= ModState.Config.SlownessLimit)
-        {
-            Game1.player.applyBuff(
-                new Buff(
-                    Constants.SlowEffectKey,
-                    duration: 60_000,
-                    iconTexture: Game1.buffsIcons,
-                    iconSheetIndex: 13,
-                    effects: new BuffEffects { Speed = { ModState.Config.SlownessAmount } },
-                    displayName: I18n.Effect_Overburdened()));
-
-            Log.Trace("Adding the slowness effect");
-            return;
-        }
-
-        if (!Game1.player.hasBuff(Constants.SlowEffectKey))
-        {
-            return;
-        }
-
-        // Remove status effect from the player
-        Game1.player.buffs.Remove(Constants.SlowEffectKey);
-        Log.Trace("Removing the slowness effect");
-    }
-
     private void OnReturnedToTitle(object? sender, ReturnedToTitleEventArgs e)
     {
         this.Helper.Events.Input.ButtonPressed -= this.OnButtonPressed;
@@ -133,6 +162,67 @@ internal sealed class ModEntry : Mod
         if (ModState.Config.SlownessLimit > 0 && ModState.Config.SlownessAmount != 0)
         {
             this.Helper.Events.GameLoop.OneSecondUpdateTicked += OnOneSecondUpdateTicked;
+        }
+
+        // Create missing backups
+        for (var i = 0; i < Game1.player.Items.Count; i++)
+        {
+            var item = Game1.player.Items[i];
+            if (item is not Chest chest)
+            {
+                if (item is null ||
+                    !item.modData.TryGetValue(Constants.BetterChestsGlobalInventoryKey, out var id) ||
+                    !Game1.player.team.globalInventories.ContainsKey(id))
+                {
+                    continue;
+                }
+
+                // Attempt to restore a Better Chest proxy
+                var color = Color.Black;
+                if (item.modData.TryGetValue(Constants.BetterChestsColorKey, out var colorString) &&
+                    int.TryParse(colorString, out var colorValue))
+                {
+                    var r = (byte)(colorValue & 0xFF);
+                    var g = (byte)((colorValue >> 8) & 0xFF);
+                    var b = (byte)((colorValue >> 16) & 0xFF);
+                    color = new Color(r, g, b);
+                }
+
+                chest = new Chest(true, item.ItemId)
+                {
+                    GlobalInventoryId = id,
+                    fridge = { Value = item.modData.ContainsKey(Constants.BetterChestsFridgeKey) },
+                    playerChoiceColor = { Value = color }
+                };
+
+                chest.CopyFieldsFrom(item);
+                _ = chest.modData.Remove(Constants.BetterChestsFridgeKey);
+                _ = chest.modData.Remove(Constants.BetterChestsColorKey);
+                _ = chest.modData.Remove(Constants.BetterChestsGlobalInventoryKey);
+                Game1.player.Items[i] = chest;
+            }
+
+            _ = ModState.Backups.TryAddBackup(chest, Constants.Prefix);
+        }
+
+        // Create generic backups for any missing
+        foreach (var (id, _) in Game1.player.team.globalInventories.Pairs)
+        {
+            // Only create backups for known ids
+            if (!id.StartsWith(Constants.Prefix, StringComparison.OrdinalIgnoreCase) &&
+                !id.StartsWith(Constants.BetterChestsPrefix, StringComparison.OrdinalIgnoreCase))
+            {
+                continue;
+            }
+
+            // Do not create if backup already exists
+            if (ModState.Backups.OfType<Chest>()
+                .Any(chest => chest.GlobalInventoryId.Equals(id, StringComparison.OrdinalIgnoreCase)))
+            {
+                continue;
+            }
+
+            ModState.Backups.Add(new Chest(true) { GlobalInventoryId = id });
         }
     }
 }
